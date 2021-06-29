ad4d31d4c7934b8a52e610dac2e9ed2bec9fe069
DucNhatNguyen <44280301+DucNhatNguyen@users.noreply.github.com>
2021-06-29 17:57:34 +0700

    create layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-trungquandev-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/spirit/18746ed563cbe964e146d69d8e72f5ff/unfurl.png" alt="ducnhatdev-img" />
+              Title: DUCNHATDEV
+            </li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+            <li>hahahahahahahahha</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
